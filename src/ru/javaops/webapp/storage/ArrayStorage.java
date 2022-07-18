2f18a8a1e96b8a469c9ce0ae10ff0b77cecb398c
unknown <balcon@inbox.ru>
2022-07-18 20:26:25 +0300

    Modify clear()

--- src/ru/javaops/webapp/storage/ArrayStorage.java
+++ src/ru/javaops/webapp/storage/ArrayStorage.java
@@ -24,9 +24,7 @@ public class ArrayStorage {
     }
 
     public void clear() {
-        for (int i = 0; i < size; i++) {
-            storage[i] = null;
-        }
+        Arrays.fill(storage, 0, size, null);
         size = 0;
     }
 
