0e520b66741b10b68907a0b076a611b01e02ffd0
unknown <balcon@inbox.ru>
2022-07-25 23:02:49 +0300

    Try reflections

---
+++ src/ru/javaops/webapp/MainReflection.java
@@ -0,0 +1,24 @@
+package ru.javaops.webapp;
+
+import ru.javaops.webapp.model.Resume;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class MainReflection {
+    public static void main(String[] args) throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+        Resume resume = new Resume("uuid1");
+
+        // Resume fields
+        Field[] resumeFields = resume.getClass().getDeclaredFields();
+        for (Field field : resumeFields) {
+            field.setAccessible(true);
+            System.out.println(field.getType() + " " + field.getName() + " : " + field.get(resume));
+        }
+
+        // invoke toString()
+        Method resumeToString = resume.getClass().getDeclaredMethod("toString");
+        System.out.println(resumeToString.invoke(resume));
+    }
+}
