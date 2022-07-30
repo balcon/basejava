378ede9afe934c3220e4944d8e2f3c8daf3da735
unknown <balcon@inbox.ru>
2022-07-30 03:30:25 +0300

    ListStorage (ugly) completed

--- src/ru/javaops/webapp/storage/ListStorage.java
+++ src/ru/javaops/webapp/storage/ListStorage.java
@@ -1,45 +1,60 @@
 package ru.javaops.webapp.storage;
 
+import ru.javaops.webapp.exception.ExistsStorageException;
+import ru.javaops.webapp.exception.NotExistsStorageException;
 import ru.javaops.webapp.model.Resume;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class ListStorage extends AbstractStorage{
-    private final List<Resume> storage=new ArrayList<>();
+public class ListStorage extends AbstractStorage {
+    private final List<Resume> storage = new ArrayList<>();
 
     @Override
     public void clear() {
-
+        storage.clear();
     }
 
     @Override
     public void save(Resume resume) {
-
+        if (storage.contains(resume)) {
+            throw new ExistsStorageException(resume);
+        }
+        storage.add(resume);
     }
 
     @Override
     public Resume get(String uuid) {
-        return null;
+        int index = storage.indexOf(new Resume(uuid));
+        if (index == -1) {
+            throw new NotExistsStorageException(new Resume(uuid));
+        }
+        return storage.get(index);
     }
 
     @Override
     public void update(Resume resume) {
-
+        int index = storage.indexOf(resume);
+        if (index == -1) {
+            throw new NotExistsStorageException(resume);
+        }
+        storage.set(index, resume);
     }
 
     @Override
     public void delete(String uuid) {
-
+        if (!storage.remove(new Resume(uuid))) {
+            throw new NotExistsStorageException(new Resume(uuid));
+        }
     }
 
     @Override
     public Resume[] getAll() {
-        return new Resume[0];
+        return storage.toArray(new Resume[0]);
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return storage.size();
     }
 }
--- test/ru/javaops/webapp/storage/ListStorageTest.java
+++ test/ru/javaops/webapp/storage/ListStorageTest.java
@@ -1,35 +1,7 @@
 package ru.javaops.webapp.storage;
 
-import org.junit.Before;
-import org.junit.Test;
-import ru.javaops.webapp.model.Resume;
-
-import static org.junit.Assert.assertEquals;
-
-public class ListStorageTest {
-    private static final String UUID_1 = "uuid1";
-    private static final String UUID_2 = "uuid2";
-    private static final String UUID_3 = "uuid3";
-    private static final String UUID_4 = "uuid4";
-    private static final String UUID_NOT_EXISTS = "uuid_not_exists";
-
-    public static final Resume RESUME_1 = new Resume(UUID_1);
-    public static final Resume RESUME_2 = new Resume(UUID_2);
-    public static final Resume RESUME_3 = new Resume(UUID_3);
-    public static final Resume RESUME_4 = new Resume(UUID_4);
-
-    private final Storage storage = new ListStorage();
-
-    @Before
-    public void setUp() throws Exception {
-        storage.save(RESUME_1);
-        storage.save(RESUME_2);
-        storage.save(RESUME_3);
+public class ListStorageTest extends AbstractArrayStorageTest {
+    public ListStorageTest() {
+        super(new ListStorage());
     }
-
-    @Test
-    public void getSize() {
-        assertEquals(3,storage.getSize());
-    }
-
 }
