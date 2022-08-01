6b1869c30ca7fdf2ab7d73d524526b1d8e725ab7
balcon <balcon@inbox.ru>
2022-08-01 17:07:48 +0300

    Exception signature changed

--- src/ru/javaops/webapp/exception/ExistsStorageException.java
+++ src/ru/javaops/webapp/exception/ExistsStorageException.java
@@ -1,9 +1,7 @@
 package ru.javaops.webapp.exception;
 
-import ru.javaops.webapp.model.Resume;
-
 public class ExistsStorageException extends StorageException {
-    public ExistsStorageException(Resume resume) {
-        super("Resume [" + resume + "] already exists");
+    public ExistsStorageException(String uuid) {
+        super("Resume [" + uuid + "] already exists");
     }
 }
--- src/ru/javaops/webapp/exception/NotExistsStorageException.java
+++ src/ru/javaops/webapp/exception/NotExistsStorageException.java
@@ -1,9 +1,7 @@
 package ru.javaops.webapp.exception;
 
-import ru.javaops.webapp.model.Resume;
-
 public class NotExistsStorageException extends StorageException {
-    public NotExistsStorageException(Resume resume) {
-        super("Resume [" + resume + "] not exists");
+    public NotExistsStorageException(String uuid) {
+        super("Resume [" + uuid + "] not exists");
     }
 }
