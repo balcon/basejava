c06b8b0ab26b9057785b2f5a1525ee0467426907
balcon <balcon@inbox.ru>
2022-08-08 22:22:26 +0300

    Add @Suite and @RunWith to tests

---
+++ test/ru/javaops/webapp/storage/StorageTests.java
@@ -0,0 +1,15 @@
+package ru.javaops.webapp.storage;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        ArrayStorageTest.class,
+        SortedArrayStorageTest.class,
+        ListStorageTest.class,
+        UuidMapStorageTest.class,
+        ResumeMapStorageTest.class
+})
+public class StorageTests {
+}
