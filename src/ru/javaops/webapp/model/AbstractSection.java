a4cb98fb884dd096d20ed9e267beee1f3367f0bc
Balykov K <balcon@inbox.ru>
2022-08-18 17:04:20 +0300

    Remove title in AbstractSection

--- src/ru/javaops/webapp/model/AbstractSection.java
+++ src/ru/javaops/webapp/model/AbstractSection.java
@@ -1,13 +1,4 @@
 package ru.javaops.webapp.model;
 
 public abstract class AbstractSection {
-    private final String title;
-
-    public AbstractSection(String title) {
-        this.title = title;
-    }
-
-    public String getTitle() {
-        return title;
-    }
 }
