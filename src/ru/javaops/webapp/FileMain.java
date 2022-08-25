72704761bc582ce44018f650c200c3c886b90403
Balykov K <balcon@inbox.ru>
2022-08-25 10:18:40 +0300

    add maringn-left

--- src/ru/javaops/webapp/FileMain.java
+++ src/ru/javaops/webapp/FileMain.java
@@ -4,12 +4,16 @@ import java.io.File;
 
 @SuppressWarnings("all")
 public class FileMain {
+    private static String marignLeft = "";
+
     static void get(File file) {
-        System.out.println(file.getPath());
+        System.out.println(marignLeft + file.getName());
         if (file.isDirectory()) {
+            marignLeft += "  ";
             for (File f : file.listFiles()) {
                 get(f);
             }
+            marignLeft = marignLeft.substring(0, marignLeft.length() - 2);
         }
     }
 
