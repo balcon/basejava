caead8ad0c92dc7a71461548cc7fd1bfb8ca18dc
Konstantin Balykov <balcon@inbox.ru>
2022-08-31 18:58:14 +0300

    Clear storage before tests

--- test/ru/javaops/webapp/storage/ObjectStreamFileStorageTest.java
+++ test/ru/javaops/webapp/storage/ObjectStreamFileStorageTest.java
@@ -1,11 +1,19 @@
 package ru.javaops.webapp.storage;
 
 import org.junit.After;
+import org.junit.Before;
 
 public class ObjectStreamFileStorageTest extends AbstractStorageTest {
 
     public ObjectStreamFileStorageTest() {
         super(new FileStorage("storage_test", new ObjectStreamSerialization()));
+    }
+
+    @Override
+    @Before
+    public void setUp() {
+        tearDown();
+        super.setUp();
     }
 
     @After
--- test/ru/javaops/webapp/storage/ObjectStreamPathStorageTest.java
+++ test/ru/javaops/webapp/storage/ObjectStreamPathStorageTest.java
@@ -1,11 +1,19 @@
 package ru.javaops.webapp.storage;
 
 import org.junit.After;
+import org.junit.Before;
 
 public class ObjectStreamPathStorageTest extends AbstractStorageTest {
 
     public ObjectStreamPathStorageTest() {
         super(new PathStorage("storage_test", new ObjectStreamSerialization()));
+    }
+
+    @Override
+    @Before
+    public void setUp() {
+        tearDown();
+        super.setUp();
     }
 
     @After
