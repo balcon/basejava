ad6a1655bb19fc620e14e9415bf83f793b85efa2
Konstantin Balykov <balcon@inbox.ru>
2022-09-29 17:09:16 +0300

    Create getAll page

--- src/ru/javaops/webapp/storage/SqlStorage.java
+++ src/ru/javaops/webapp/storage/SqlStorage.java
@@ -12,6 +12,11 @@ public class SqlStorage implements Storage {
     private final SqlHelper sqlHelper;
 
     public SqlStorage(String url, String user, String password) {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
         sqlHelper = new SqlHelper(() -> DriverManager.getConnection(url, user, password));
     }
 
--- src/ru/javaops/webapp/web/ResumeServlet.java
+++ src/ru/javaops/webapp/web/ResumeServlet.java
@@ -1,16 +1,27 @@
 package ru.javaops.webapp.web;
 
-import javax.servlet.ServletException;
+import ru.javaops.webapp.model.Resume;
+import ru.javaops.webapp.storage.util.Config;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 public class ResumeServlet extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        List<Resume> resumes = Config.get().getStorage().getAllSorted();
+        StringBuilder str = new StringBuilder();
+        str.append("<table border=1><tr><th>Uuid</th><th>Full name</th></tr>");
+        for (Resume resume : resumes) {
+            str.append("<tr><td>").append(resume.getUuid()).append("</td>");
+            str.append("<td>").append(resume.getFullName()).append("</td></tr>");
+        }
+        str.append("</table>");
+
         resp.setContentType("text/html;charset=utf-8");
-        resp.getWriter().write("");
+        resp.getWriter().write(str.toString());
     }
 }
 
